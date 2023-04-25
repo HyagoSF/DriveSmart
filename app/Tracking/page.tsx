faad7257523db8685779d75632f6f2081259c355
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2023-04-25 14:34:03 -0400

    First look for initial page

---
+++ app/Tracking/page.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import { start } from 'repl';
+import React, { useState } from 'react';
+
+export default function Tracking() {
+	let watchId: any = null;
+	let startPosition: any = null;
+	let totalDistance = 0;
+	let startTime: any = null;
+	const [location, setLocation] = useState({ lat: 0, lon: 0 });
+
+	function showMeMyLocation() {
+		navigator.geolocation.getCurrentPosition(
+			function (position) {
+				setLocation({
+					lat: position.coords.latitude,
+					lon: position.coords.longitude,
+				});
+				console.log('Latitude is :', position.coords.latitude);
+				console.log('Longitude is :', position.coords.longitude);
+			},
+			function (error) {
+				console.error(
+					'Error Code = ' + error.code + ' - ' + error.message
+				);
+			}
+		);
+	}
+
+	function startTracking() {
+		startPosition = null;
+		totalDistance = 0;
+		startTime = null;
+		console.log('Starting counting distance and time');
+		watchId = navigator.geolocation.watchPosition(
+			function (position) {
+				if (!startPosition) {
+					startPosition = position;
+					startTime = new Date().getTime();
+				} else {
+					const distance = getDistanceFromLatLonInKm(
+						startPosition.coords.latitude,
+						startPosition.coords.longitude,
+						position.coords.latitude,
+						position.coords.longitude
+					);
+					totalDistance += distance;
+					startPosition = position;
+				}
+			},
+			function (error) {
+				console.error(error);
+			},
+			{
+				enableHighAccuracy: true,
+				maximumAge: 0,
+				timeout: 5000,
+			}
+		);
+
+		// console.log(startTime);
+	}
+
+	function stopTracking() {
+		navigator.geolocation.clearWatch(watchId);
+		watchId = null;
+		const endTime = new Date().getTime();
+		const hours = (endTime - startTime) / (1000 * 60 * 60);
+		const minutes = Math.floor(hours * 60);
+		const seconds = Math.floor((hours * 60 - minutes) * 60);
+		console.log(
+			`You drove ${totalDistance.toFixed(
+				2
+			)} kilometers in ${hours.toFixed(
+				2
+			)} hours, ${minutes} minutes and ${seconds} seconds.`
+		);
+	}
+
+	function getDistanceFromLatLonInKm(
+		lat1: any,
+		lon1: number,
+		lat2: number,
+		lon2: number
+	) {
+		const earthRadiusKm = 6371;
+		const dLat = deg2rad(lat2 - lat1);
+		const dLon = deg2rad(lon2 - lon1);
+		const a =
+			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+			Math.cos(deg2rad(lat1)) *
+				Math.cos(deg2rad(lat2)) *
+				Math.sin(dLon / 2) *
+				Math.sin(dLon / 2);
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		const distance = earthRadiusKm * c;
+		return distance;
+	}
+
+	function deg2rad(deg: any) {
+		return deg * (Math.PI / 180);
+	}
+
+	const Button = ({ onClick, className, children }) => (
+		<button onClick={onClick} className={className}>
+			{children}
+		</button>
+	);
+
+	return (
+		<div className="">
+			<h1>
+				This is how many km you walked:
+				{totalDistance.toFixed(2)}
+			</h1>
+
+			<div className="">
+				<h1>
+					Your current location is:
+					{location.lat}, {location.lon}
+				</h1>
+			</div>
+
+			{/* Create buttons to start tracking */}
+			<Button onClick={startTracking} className={''}>
+				Start
+			</Button>
+			<Button onClick={stopTracking} className={`ml-10`}>
+				Stop
+			</Button>
+			<Button onClick={showMeMyLocation} className={`ml-10`}>
+				Give me my current location
+			</Button>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,106 @@
 import Link from 'next/link';
+import {
+	User,
+	BellIcon,
+	Car,
+	ChevronDown,
+	Home as HomeIcon,
+	CalendarSearch,
+	DollarSign,
+} from 'lucide-react';
 
 export default function Home() {
 	return (
-		<main>
-			<nav className="">
-				<ul className="flex flex-center justify-end gap-4 p-4 text-gray-500 w-screen">
+		<main className="bg-gray-200 min-h-screen">
+			{/* NavBar */}
+			<nav
+				className="flex flex-col justify-center bg-white sticky top-0 z-50
+			">
+				{/* <ul className="flex flex-center justify-between w-screen px-4 py-2">
 					<Link href="#">Home</Link>
 					<Link href="About">About</Link>
-				</ul>
+					<Link href="Tracking">Tracking</Link>
+				</ul> */}
+
+				<div className="flex flex-row items-center justify-between m-4">
+					<User
+						color="white"
+						size={32}
+						className=" bg-gray-600 rounded-full"
+					/>
+
+					{/* User */}
+					<div className="flex bg-gray-100 rounded p-1">
+						<h1 className="text-xl">Hyago</h1>
+
+						{/* Dropdown */}
+
+						{/* TODO: MAKE THIS BUTTON POSSIBLE TO CHANGE THE USER
+
+						WHEN CLICK: WILL GENERATE A LIST OF USERS TO CHANGE
+						*/}
+
+						<button>
+							<ChevronDown color="black" size={24} className="" />
+						</button>
+					</div>
+
+					<BellIcon color="black" size={32} className="" />
+				</div>
 			</nav>
 
-			<h1 className={`font-open-sans`}>inter</h1>
-			<h1 className={`font-open-sans`}>test</h1>
-			<h1 className={``}>inter</h1>
+			{/* Content */}
+
+			{/* Tracking option */}
+			<div className="flex flex-row items-center bg-white m-2 rounded">
+				{/* TODO: MAKE THIS CAR GO TO THE RIGHT WHEN THE USER CLICK TO START DRIVE, AND MAKE SOMETHING GREEN */}
+
+				<div className="flex flex-row justify-between items-center gap-16 mx-1">
+					<div className="flex flex-row items-center m-2 gap-2">
+						<Car color="black" size={32} className="" />
+						<button className="">Start Drive</button>
+					</div>
+
+					{/* TODO: THIS WILL BE FUNCTIONS, WILL UPDATE BY ITSELF */}
+					<h3>0:30 - 11km</h3>
+				</div>
+			</div>
+
+			<div className=" bg-white m-2 rounded h-screen">
+				<h1 className="text-2xl">This is the home page</h1>
+
+				<div className="">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing Lorem
+						ipsum dolor, sit amet consectetur adipisicing elit.
+						Facilis, expedita cum deleniti nostrum voluptas soluta
+						tempora quia et recusandae voluptate sed reprehenderit
+						ab magni provident voluptatibus, doloribus molestias
+						consectetur necessitatibus. elit. Ad ipsa ut, ipsum
+						repudiandae, beatae quam at modi
+					</p>
+				</div>
+			</div>
+
+			{/* Footer */}
+			<footer className="flex flex-col items-center justify-center w-screen h-16 bg-white sticky bottom-0 ">
+				<ul className="flex flex-center justify-between w-screen px-4 py-2 ">
+					<Link href="#" className="flex flex-col items-center">
+						<HomeIcon size={32} />
+						Home
+					</Link>
+					<Link href="About" className="flex flex-col items-center">
+						<CalendarSearch size={32} />
+						Events
+					</Link>
+					<Link
+						href="Tracking"
+						className="flex flex-col items-center">
+						<DollarSign size={32} />
+						Earnings
+					</Link>
+				</ul>
+			</footer>
 		</main>
 	);
 }
