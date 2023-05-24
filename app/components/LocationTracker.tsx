dd1cbe6770149f98a0618c38eb7fd29dc52b5f1a
Hyago Ferreira <hyagosilvaferreira@gmail.com>
2023-05-24 13:45:45 -0400

    Starting using google maps API to get the distance between some points

--- app/components/LocationTracker.tsx
+++ app/components/LocationTracker.tsx
@@ -1,127 +1,171 @@
-import React, { useState, useEffect } from 'react';
+'use client';
 
-const LocationTracker = () => {
-	const [position, setPosition] = useState(null);
+import { useEffect, useState } from 'react';
+
+const coordinates = [
+	{ lat: 43.629, lng: -79.489 },
+	{ lat: 43.624, lng: -79.515 },
+	{ lat: 43.639, lng: -79.521 },
+];
+
+interface locationType {
+	lat: number;
+	lng: number;
+}
+
+export default function MileageTracker() {
+	const [map, setMap] = useState(null);
+	const [directionsService, setDirectionsService] = useState(null);
+	const [directionsRenderer, setDirectionsRenderer] = useState(null);
+	const [startLocation, setStartLocation] = useState<locationType>({
+		lat: 43.629,
+		lng: -79.489,
+	});
+	const [endLocation, setEndLocation] = useState<locationType | null>(null);
 	const [distance, setDistance] = useState(0);
+	const [trackedLocations, setTrackedLocations] = useState<locationType[]>(
+		[]
+	);
 
-	const calculateDistance = (lat1, lon1, lat2, lon2) => {
-		const R = 6371e3; // radius of the earth in meters
-		const phi1 = lat1 * (Math.PI / 180);
-		const phi2 = lat2 * (Math.PI / 180);
-		const deltaPhi = (lat2 - lat1) * (Math.PI / 180);
-		const deltaLambda = (lon2 - lon1) * (Math.PI / 180);
-
-		const a =
-			Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
-			Math.cos(phi1) *
-				Math.cos(phi2) *
-				Math.sin(deltaLambda / 2) *
-				Math.sin(deltaLambda / 2);
-		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		return R * c;
+	const mapOptions = {
+		center: { lat: 43.629, lng: -79.489 },
+		zoom: 10,
 	};
 
+	// This will run just once
 	useEffect(() => {
-		const watchId = navigator.geolocation.watchPosition(
-			(position) => {
-				if (
-					position.coords &&
-					position.coords.accuracy < 100 &&
-					position.coords.speed? > 0 &&
-					position.timestamp > 0 &&
-					position.coords.latitude !== position.coords.longitude &&
-					position.coords.latitude !== null &&
-					position.coords.longitude !== null &&
-					position.coords.latitude !== undefined &&
-					position.coords.longitude !== undefined &&
-					position.coords.latitude !== 0 &&
-					position.coords.longitude !== 0 &&
-					!isNaN(position.coords.latitude) &&
-					!isNaN(position.coords.longitude) &&
-					position.coords.latitude !== Infinity &&
-					position.coords.longitude !== Infinity &&
-					position.coords.latitude !== -Infinity &&
-					position.coords.latitude !== 0.0 &&
-					position.coords.longitude !== 0.0 &&
-					!position.coords.heading &&
-					!position.coords.altitude
-				) {
-					const { latitude, longitude } = position.coords;
-					setPosition({ latitude, longitude });
-				}
-			},
-			(error) => {
-				console.log(error);
-			},
-			{
-				enableHighAccuracy: true,
-				timeout: 20000,
-				maximumAge: 1000,
-				distanceFilter: 10, // minimum distance (in meters) between updates
-			}
-		);
+		const mapScript = document.createElement('script');
+		mapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCpMEKoIbnf0FIGlcl9-mM8WyNeCfJB7Js&libraries=places`;
+
+		mapScript.onload = initMap;
+		document.head.appendChild(mapScript);
 
 		return () => {
-			navigator.geolocation.clearWatch(watchId);
+			document.head.removeChild(mapScript);
 		};
 	}, []);
 
+	// This will run every time the trackedLocations array changes
 	useEffect(() => {
-		if (position) {
-			const { latitude: lat1, longitude: lon1 } = position;
-			const intervalId = setInterval(() => {
-				navigator.geolocation.getCurrentPosition(
-					(position) => {
-						if (
-							position.coords &&
-							!isNaN(position.coords.latitude) &&
-							!isNaN(position.coords.longitude)
-						) {
-							const { latitude: lat2, longitude: lon2 } =
-								position.coords;
-							const d = calculateDistance(lat1, lon1, lat2, lon2);
-							setDistance((prevDistance) => prevDistance + d);
-							setPosition({ latitude: lat2, longitude: lon2 });
-						}
-					},
-					(error) => {
-						console.log(error);
-					},
-					{
-						enableHighAccuracy: true,
-						timeout: 20000,
-						maximumAge: 100,
-					}
-				);
-			}, 10000); // update distance every 10 seconds
-
-			return () => {
-				clearInterval(intervalId);
-			};
+		if (trackedLocations.length === coordinates.length) {
+			setEndLocation(trackedLocations[trackedLocations.length - 1]);
+			handleStartTracking();
 		}
-	}, [position]);
+	}, [trackedLocations]);
 
-	const handleSaveDistance = async () => {
-		// send distance to server to save in database
-		const response = await fetch('/api/save-distance', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ distance }),
-		});
-		if (response.ok) {
-			console.log('Distance saved successfully');
+	const initMap = () => {
+		const mapInstance = new google.maps.Map(
+			document.getElementById('map'),
+			mapOptions
+		);
+		setMap(mapInstance);
+		setDirectionsService(new google.maps.DirectionsService());
+		setDirectionsRenderer(
+			new google.maps.DirectionsRenderer({ map: mapInstance })
+		);
+
+		if (navigator.geolocation) {
+			startTracking();
 		} else {
-			console.log('Error saving distance');
+			console.error('Geolocation is not supported by this browser.');
+		}
+	};
+
+	// const startTracking = () => {
+	// 	if (tracking) {
+	// 		setInterval(() => {
+	// 			navigator.geolocation.getCurrentPosition((position) => {
+	// 				const { latitude, longitude } = position.coords;
+	// 				const newLocation = { lat: latitude, lng: longitude };
+
+	// 				setTrackedLocations((prevLocations) => [
+	// 					...prevLocations,
+	// 					newLocation,
+	// 				]);
+
+	// 				if (trackedLocations.length === 10) {
+	// 					handleStartTracking();
+	// 				}
+	// 			});
+	// 		}, 10000);
+	// 	}
+	// };
+
+	const startTracking = () => {
+		let currentIndex = 0;
+		const intervalId = setInterval(() => {
+			const currentCoordinate = coordinates[currentIndex];
+
+			const newLocation = {
+				lat: currentCoordinate.lat,
+				lng: currentCoordinate.lng,
+			};
+			setTrackedLocations((prevLocations) => [
+				...prevLocations,
+				newLocation,
+			]);
+
+			console.log(currentIndex);
+			console.log(coordinates.length - 1);
+
+			// just run this until the end of the array coordinates
+			if (currentIndex === coordinates.length - 1) {
+				setEndLocation(newLocation);
+				clearInterval(intervalId); // stop the interval
+			}
+
+			currentIndex++;
+		}, 50000);
+	};
+
+	const calculateDistance = (response) => {
+		if (response.routes && response.routes.length > 0) {
+			const leg = response.routes[0].legs[0];
+			setDistance((prevDistance) => prevDistance + leg.distance.value);
+			console.log('Driven distance:', leg.distance.text);
+		} else {
+			console.error('No route found.');
+		}
+	};
+
+	const handleStartTracking = () => {
+		console.log('end location: ', endLocation);
+
+		if (startLocation && endLocation) {
+			const request = {
+				origin: startLocation,
+				destination: endLocation,
+				waypoints: trackedLocations.slice(1, -1).map((location) => ({
+					location: new google.maps.LatLng(
+						location.lat,
+						location.lng
+					),
+				})),
+				travelMode: 'DRIVING',
+			};
+
+			directionsService.route(request, (result, status) => {
+				if (status === 'OK') {
+					directionsRenderer.setDirections(result);
+					calculateDistance(result);
+					setStartLocation(trackedLocations.slice(-1)[0]);
+					setTrackedLocations([]);
+				} else {
+					console.error('Directions request failed:', status);
+				}
+			});
+		} else {
+			console.error(
+				'Start and end locations are required, and  tracked locations are needed.'
+			);
 		}
 	};
 
 	return (
 		<div>
-			<p>Distance traveled: {distance} meters</p>
-			<button onClick={handleSaveDistance}>Save distance</button>
+			<div id="map" style={{ width: '0%', height: '400px' }}></div>
+
+			<div>Distance: {distance} meters</div>
 		</div>
 	);
-};
-export default LocationTracker;
+}
